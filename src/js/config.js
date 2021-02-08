5092cf2f05ff734059d8fec4a19d2cfc1089015d
Calvin Roberts <roberts.calvin85@gmail.com>
2021-02-08 14:52:46 +0000

    Uploading a New Recipe - Part 2.

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '921a23b6-9f81-4f6a-8ee0-39dd74a3b9b7';
+export const MODAL_CLOSE_SEC = 2.5;
